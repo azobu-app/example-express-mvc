3eaaae687807c6da6abc4157d901e70f0c8e6c7e
M Haidar Hanif <git@mhaidarhanif.com>
2020-05-07 16:09:45 +0800

    Improve root API response

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
+  const baseUrl = req.protocol + '://' + req.get('host') + req.originalUrl
+
   res.send({
     title: 'Example Express',
-    message: 'Welcome to Example Express!',
+    author: 'Azobu App',
+    message: 'Welcome to Example Express API!',
+    baseUrl: baseUrl,
   })
 })
 
